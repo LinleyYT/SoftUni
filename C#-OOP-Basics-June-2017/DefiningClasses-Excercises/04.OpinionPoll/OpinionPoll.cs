538a6ca6e22765d3a6fda8418e97e1504381895e
georgiab23 <alexander.b.georgiev@gmail.com>
2017-06-30 12:40:43 +0300

    Commit exercise 4

---
+++ C#-OOP-Basics-June-2017/DefiningClasses-Excercises/04.OpinionPoll/OpinionPoll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class OpinionPoll
+{
+    public static void Main()
+    {
+        var people = new List<Person>();
+        var n = int.Parse(Console.ReadLine().Trim());
+
+        for (int i = 0; i < n; i++)
+        {
+            var inputArgs = Console.ReadLine().Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var name = inputArgs[0];
+            var age = int.Parse(inputArgs[1]);
+
+            var currentPerson = new Person(name, age);
+            people.Add(currentPerson);
+        }
+
+        foreach (var person in people.Where(x => x.Age > 30).OrderBy(y => y.Name))
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
+
