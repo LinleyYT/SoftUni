c5653adbb4ec8ce53195ab0c95183c1f4f12383b
georgiab23 <alexander.b.georgiev@gmail.com>
2017-07-01 12:45:18 +0300

    Up to exercise 9

---
+++ C#-OOP-Basics-June-2017/DefiningClasses-Excercises/09.RectangleIntersection/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.RectangleIntersection
+{
+    public class Rectangle
+    {
+        public string ID { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double[] Coordinates { get; set; }
+
+        public Rectangle(string ID, double width, double height, double[] coords)
+        {
+            this.ID = ID;
+            this.Width = width;
+            this.Height = height;
+            this.Coordinates = coords;
+        }
+
+        public bool RectangleIntersect(Rectangle rectangle)
+        {
+            if (rectangle.Coordinates[0] <= this.Coordinates[0] + this.Width && rectangle.Coordinates[0] + rectangle.Width >= this.Coordinates[0] && rectangle.Coordinates[1] <= this.Coordinates[1] + this.Height && rectangle.Coordinates[1] + rectangle.Height >= this.Coordinates[1])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C#-OOP-Basics-June-2017/DefiningClasses-Excercises/09.RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.RectangleIntersection
+{
+    public class RectangleIntersection
+    {
+        public static void Main()
+        {
+            var specs = Console.ReadLine().Trim()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var n = specs[0];
+            var m = specs[1];
+
+            var rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var rectangleSpecs = Console.ReadLine().Trim()
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var id = rectangleSpecs[0];
+                var width = double.Parse(rectangleSpecs[1]);
+                var height = double.Parse(rectangleSpecs[2]);
+                var coords = new double[2] { double.Parse(rectangleSpecs[3]), double.Parse(rectangleSpecs[4])};
+
+                var currentRect = new Rectangle(id, width, height, coords);
+                rectangles.Add(currentRect);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                var IDs = Console.ReadLine().Trim()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var firstID = IDs[0];
+                var secondID = IDs[1];
+
+                var rectOne = rectangles.Where(x => x.ID == firstID).FirstOrDefault();
+                var rectTwo = rectangles.Where(x => x.ID == secondID).FirstOrDefault();
+
+                var result = rectOne.RectangleIntersect(rectTwo);
+
+                if (result)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
