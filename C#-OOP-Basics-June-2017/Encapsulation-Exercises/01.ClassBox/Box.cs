f3c100da77323c5bd0f9c9ebf9ce8f2fdadeee54
georgiab23 <alexander.b.georgiev@gmail.com>
2017-07-02 17:38:04 +0300

    Encapsulation-Exercises up to Problem 3

---
+++ C#-OOP-Basics-June-2017/Encapsulation-Exercises/01.ClassBox/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public void GetSurfaceArea()
+        {
+            var surfaceArea = 2 * this.length * this.width + 2 * this.length * this.height +
+                              2 * this.width * this.height;
+            Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+        }
+
+        public void GetLateralSurfaceArea()
+        {
+            var laterSurfaceArea = 2 * this.length * this.height +
+                                   2 * this.width * this.height;
+            Console.WriteLine($"Lateral Surface Area - {laterSurfaceArea:F2}");
+        }
+
+        public void GetVolume()
+        {
+            var volume = this.length * this.width * this.height;
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+
+    }
+}
---
+++ C#-OOP-Basics-June-2017/Encapsulation-Exercises/02.ClassBoxDataValidation/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.ClassBoxDataValidation
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(length, width, height);
+                box.GetSurfaceArea();
+                box.GetLateralSurfaceArea();
+                box.GetVolume();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+        }
+    }
+}
